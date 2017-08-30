72fa2f093494327085fee55d7088d7dc27e15137
Nicolás Fernández <nicof@gmail.com>
2017-08-30 20:29:59 -0300

    Agrego campos en la tabla Carrera (carrera_abreviatura), y en la tabla Plan (varios campos). Agrego script de INSERTS en la base para las carreras (11) y para los planes (14) y los agrego a la base de datos localhost con el script.

--- horas/models.py
+++ horas/models.py
@@ -27,6 +27,7 @@ class Sede (models.Model):
 
 class Carrera (models.Model):
     carrera_nombre = models.CharField(max_length=230, unique=True)
+    carrera_abreviada = models.CharField(max_length=10, unique=True, blank=True, null=True)
     sedes = models.ManyToManyField(Sede)
 
     class Meta:
@@ -38,6 +39,13 @@ class Carrera (models.Model):
 class Plan (models.Model):
     carrera = models.ForeignKey(Carrera)
     plan_nombre = models.CharField(max_length=50)
+    plan_version = models.IntegerField(blank=True, null=True, default=1)
+    resolucion_fecha = models.DateField(blank=True, null=True)
+    resolucion_numero = models.IntegerField(blank=True, null=True)
+    total_horas = models.IntegerField(blank=True, null=True)
+    cant_materias = models.IntegerField(blank=True, null=True, default=0)
+    cant_materias_optativas = models.IntegerField(blank=True, null=True, default=0)
+    cant_materias_idiomas = models.IntegerField(blank=True, null=True, default=0)
 
     class Meta:
         verbose_name_plural = 'Planes'
@@ -59,20 +67,11 @@ class Periodo (models.Model):
 
 
 class Materia (models.Model):
-    ANIOS_ACAD = (
-        (1, '1er Año'),
-        (2, '2do Año'),
-        (3, '3er Año'),
-        (4, '4to Año'),
-        (5, '5to Año'),
-    )
     materia_nombre = models.CharField(max_length=100)
     plan = models.ForeignKey(Plan)
     hs_semanales = models.IntegerField(null=True)
     hs_total_materia = models.IntegerField(null=True)
-    anio_academico = models.IntegerField(
-        choices=ANIOS_ACAD,
-    )
+    anio_academico = models.IntegerField(null=True)
     periodo = models.ForeignKey(Periodo)
 
     class Meta:
@@ -93,32 +92,16 @@ class TipoDocente (models.Model):
         return self.tipo_docente
 
 class Persona (models.Model):
-    ESTADO = (
-        ('0', 'Inactivo'),
-        ('1', 'Activo'),
-    )
-    ARRAY_SEXO = (
-        ('Hombre', 'Hombre'),
-        ('Mujer', 'Mujer'),
-    )
     cuil = models.CharField('C.U.I.L.', max_length=11, unique=True)
     apellidos = models.CharField('Apellido/s', max_length=40)
     nombres = models.CharField('Nombre/s', max_length=40)
-    sexo = models.CharField(
-        max_length=20,
-        choices=ARRAY_SEXO,
-        default='Hombre',
-    )
-    legajo_numero = models.IntegerField('Legajo número',unique=True)
+    sexo = models.CharField(max_length=20, default='Sin Especificar')
+    legajo_numero = models.IntegerField('Legajo número', unique=True)
     telefono = models.CharField('Teléfono', max_length=30, blank=True, null=True,)
     email = models.EmailField('E-mail', blank=True)
     fecha_nacimiento = models.DateField('Fecha de nacimiento', blank=True, null=True,)
     antiguedad = models.IntegerField()
-    activo = models.CharField(
-        max_length=20,
-        choices=ESTADO,
-        default='1',
-    )
+    activo = models.CharField(max_length=2, default='1')
 
     class Meta:
         ordering = ['apellidos']
@@ -137,24 +120,9 @@ class Dependencia(models.Model):
         return self.dependencia_nombre
 
 class PersonaHoras (models.Model):
-    ANIOS = (
-        ('2009', '2009'),
-        ('2010', '2010'),
-        ('2011', '2011'),
-        ('2012', '2012'),
-        ('2013', '2013'),
-        ('2014', '2014'),
-        ('2015', '2015'),
-        ('2016', '2016'),
-        ('2017', '2017'),
-    )
     persona = models.ForeignKey(Persona)
     resolucion_numero = models.IntegerField('Resolución número')
-    resolucion_anio = models.CharField('Resolución año',
-        max_length=10,
-        choices=ANIOS,
-        default='2017',
-    )
+    resolucion_anio = models.CharField('Resolución año', max_length=10)
     hs_catedras = models.IntegerField('Horas cátedras')
     fecha_inicio = models.DateField('Fecha de Inicio')
     fecha_fin = models.DateField('Fecha de Fin')
@@ -164,40 +132,15 @@ class PersonaHoras (models.Model):
         verbose_name_plural = 'Persona-Horas'
 
 class DocenteHoras (models.Model):
-    ANIOS = (
-        ('2009', '2009'),
-        ('2010', '2010'),
-        ('2011', '2011'),
-        ('2012', '2012'),
-        ('2013', '2013'),
-        ('2014', '2014'),
-        ('2015', '2015'),
-        ('2016', '2016'),
-        ('2017', '2017'),
-    )
-    ASALARIADO = 'Asalariado'
-    ADHONOREM = 'Ad Honorem'
-    ARRAY_REMUNERADO = (
-        (ASALARIADO, 'Asalariado'),
-        (ADHONOREM, 'Ad Honorem'),
-    )
     persona = models.ForeignKey(Persona)
     materia = models.ForeignKey(Materia)
     sede = models.ForeignKey(Sede)
     docente_tipo = models.ForeignKey(TipoDocente)
     resolucion_numero = models.CharField('Resolución número', max_length=40)
-    resolucion_anio = models.CharField('Resolución año',
-        max_length=10,
-        choices=ANIOS,
-        default='2017',
-    )
+    resolucion_anio = models.CharField('Resolución año', max_length=10)
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
-    remunerado = models.CharField(
-        max_length=30,
-        choices=ARRAY_REMUNERADO,
-        default=ASALARIADO,
-    )
+    remunerado = models.CharField(max_length=30)
     porcentaje_aplicado = models.IntegerField(blank=True, null=True)
     hs_institucionales = models.IntegerField(blank=True, null=True)
 
