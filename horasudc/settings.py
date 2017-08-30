f43922aa47cab1ce5f9ac0114d78f889acd9599e
Nicolás Fernández <nicof@gmail.com>
2017-08-30 17:41:26 -0300

    Cambio acceso a la base postgres

--- horasudc/settings.py
+++ horasudc/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'horasudc',
-            'USER': 'postgres',
-            'PASSWORD': 'nrnf',
+            'USER': 'sistemas',
+            'PASSWORD': 's1st3m4s',
             'HOST': '127.0.0.1',
             'PORT': '5432',
     }
