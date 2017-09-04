afc4166b1fecd87e5d4e1ab6bec5b31fb23ef012
Nicolás Fernández <nicof@gmail.com>
2017-09-04 12:25:05 -0300

    Sobreescribo el settings de la aplicacion (quitando información importante).

--- horasudc/settings.py
+++ horasudc/settings.py
@@ -84,11 +84,11 @@ DATABASES = {
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'horasudc',
-            'USER': 'sistemas',
-            'PASSWORD': 's1st3m4s',
+            'NAME': '',
+            'USER': '',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
-            'PORT': '5432',
+            'PORT': '',
     }
 }
 
