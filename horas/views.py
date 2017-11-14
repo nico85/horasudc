a18c9d09c915735ed5b9907f0a3141b399bce3d5
Nicolás H. Fernández <nicof05@gmail.com>
2017-11-14 17:58:35 -0300

    6to arreglo exportar excel en administrativos: error en campos con acento

--- horas/views.py
+++ horas/views.py
@@ -544,7 +544,7 @@ def export_admin_xls(request):
     for adm in administrativos:
         apenom = adm.persona.apellidos + ', ' + adm.persona.nombres
         depend = adm.dependencia.dependencia_nombre
-        resolu = adm.resolucion_numero + '/' + adm.resolucion_anio
+        resolu = str(adm.resolucion_numero) + '/' + str(adm.resolucion_anio)
         writer.writerow([apenom, adm.persona.cuil, resolu, adm.fecha_inicio, adm.fecha_fin, adm.hs_catedras, depend])
 
     return response
